08c8dc9105d8ab46fd8dadf3834df24ea950ceae
dune123 <96974209+dune123@users.noreply.github.com>
2025-02-14 09:16:47 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/userContext.jsx";
-import CaptainContext from "./context/CaptainContext.jsx";
-import SocketProvider from "./context/SocketContext.jsx";
+import CaptainContext from "./context/captainContext.jsx";
+import SocketProvider from "./context/socketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <CaptainContext>
