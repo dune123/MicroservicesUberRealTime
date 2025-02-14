4f870d0fab0a26842a2ec6e28566b1cd6d28db8b
dune123 <96974209+dune123@users.noreply.github.com>
2025-02-14 09:20:50 +0530

    Update CaptainLogin.jsx

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios';
-import { CaptainDataContext } from '../context/CaptainContext';
+import { CaptainDataContext } from '../context/captainContext';
 
 const CaptainLogin = () => {
     const [email,setEmail]=useState('');
